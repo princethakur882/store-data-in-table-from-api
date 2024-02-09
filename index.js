5347b1ca676bcfcfd1ea0f85a2853962741afe5f
Prince <princethakur8282@gmail.com>
2024-02-09 14:51:54 +0530

    second

--- index.js
+++ index.js
@@ -1,104 +1,103 @@
 let currentPage = 1;
 let itemsPerPage = 10;
 let data = [];
+let currentSortProperty = null;
+let isSortAscending = true;
 
 function fetchPage(page) {
-    fetch("https://dummyjson.com/products")
-    .then((response) => {
-        return response.json();
-    })
-    .then((objectData) => {
-        data = objectData.products;
-        console.log(objectData);
-        let tabledata = "";
-        let start = (page - 1) * itemsPerPage;
-        let end = start + itemsPerPage;
-        data.slice(start, end).map((values) => {
-            tabledata += `<tr>
-            <td>${values.id}</td>
-            <td>${values.title}</td>
-            <td>${values.brand}</td>
-            <td>${values.description}</td>
-            <td>${values.price}</td>
-            <td>${values.category}</td>
-            </tr>`;
+    fetch("https://dummyjson.com/products?limit=100")
+        .then((response) => {
+            return response.json();
+        })
+        .then((objectData) => {
+            data = objectData.products;
+            displayTable(page);
         });
-        document.getElementById('tablebody').innerHTML = tabledata;
-    })
 }
-
-function displayData(filteredData) {
+function displayTable(page) {
     let tabledata = "";
-    filteredData.map((values) => {
+    let start = (page - 1) * itemsPerPage;
+    let end = start + itemsPerPage;
+    let slicedData = data.slice(start, end);
+
+    slicedData.map((values) => {
         tabledata += `<tr>
             <td>${values.id}</td>
-            <td>${values.title}</td>
             <td>${values.brand}</td>
+            <td>${values.title}</td>
             <td>${values.description}</td>
             <td>${values.price}</td>
             <td>${values.category}</td>
         </tr>`;
     });
+
     document.getElementById('tablebody').innerHTML = tabledata;
 }
 
-fetchPage(currentPage);
+function sortData(property) {
+    if (currentSortProperty === property) {
+        isSortAscending = !isSortAscending;
+    } else {
+        isSortAscending = true;
+        currentSortProperty = property;
+    }
+    slicedData.sort((a, b) => {
+        const valueA = a[property].brand;
+        const valueB = b[property].brand;
+
+        if (valueA < valueB) {
+            return isSortAscending ? -1 : 1;
+        }
+        if (valueA > valueB) {
+            return isSortAscending ? 1 : -1;
+        }
+        return 0;
+    });
+    displayTable(currentPage);
+}
+
+document.querySelector(".sort").addEventListener('click', function () {
+
+    let property = this.dataset.property;
+
+    document.querySelectorAll('.sort').forEach((header) => {
+        if (header !== this) {
+            header.classList.remove('ascending', 'descending');
+        }
+    });
+
+    this.classList.toggle(isSortAscending ? 'ascending' : 'descending');
+    sortData(property);
+});
+
+function filterData(searchTerm) {
+    return data.filter(item =>
+        item.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        item.brand.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        item.description.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        item.category.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+}
+
+document.getElementById('searchButton').addEventListener('click', () => {
+    let searchTerm = document.getElementById('searchBar').value;
+    let filteredData = filterData(searchTerm);
+    data = filteredData;
+    displayTable(currentPage);
+});
 
 document.getElementById('nextPage').addEventListener('click', () => {
-    if (currentPage < 3) {
+    if (currentPage < 10) {
         currentPage++;
-        fetchPage(currentPage);
+        displayTable(currentPage);
     }
 });
 
 document.getElementById('prevPage').addEventListener('click', () => {
     if (currentPage > 1) {
         currentPage--;
-        fetchPage(currentPage);
+        displayTable(currentPage);
     }
 });
 
-// Function to sort data by a given property
-function sortData(property, ascending) {
-    data.sort(function(a, b) {
-        if (ascending) {
-            return a[property] > b[property] ? 1 : -1;
-        } else {
-            return a[property] < b[property] ? 1 : -1;
-        }
-    });
-}
-
-
-
-document.querySelector(".sort").addEventListener('click', function() {
-        let ascending = this.classList.contains('ascending');
-        sortData(property, ascending);
-        // Remove 'ascending' class from all other headers
-        document.querySelector('.sort').forEach((header) => {
-            if (header !== this) {
-                header.classList.remove('ascending');
-            }
-        });
-        this.classList.toggle('ascending');
-        fetchPage(currentPage);
-    });
-
-
-function filterData(searchTerm) {
-    let filteredData = data.filter(item => 
-        item.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
-        item.brand.toLowerCase().includes(searchTerm.toLowerCase()) ||
-        item.description.toLowerCase().includes(searchTerm.toLowerCase()) ||
-        item.category.toLowerCase().includes(searchTerm.toLowerCase())
-    );
-    return filteredData;
-}
-
-document.getElementById('searchButton').addEventListener('click', () => {
-    let searchTerm = document.getElementById('searchBar').value; 
-    let filteredData = filterData(searchTerm);
-    displayData(filteredData); 
-});
-
-
+fetchPage(currentPage);
